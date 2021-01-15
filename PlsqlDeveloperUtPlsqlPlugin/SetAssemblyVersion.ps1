206929fa08a64e669f11108f9fb0aef3359b17bc
Simon Martinelli <simon@72.services>
2021-01-15 09:13:34 +0100

    Clean up

--- PlsqlDeveloperUtPlsqlPlugin/SetAssemblyVersion.ps1
+++ PlsqlDeveloperUtPlsqlPlugin/SetAssemblyVersion.ps1
@@ -1,8 +1,6 @@
 param([string]$NewVersion)
 
-Get-ChildItem -Include assemblyinfo.cs, assemblyinfo.vb -Recurse | 
-    ForEach-Object {
-        $_.IsReadOnly = $false
-        (Get-Content -Path $_) -replace '(?<=Assembly(?:File)?Version\(")[^"]*(?="\))', $NewVersion |
-            Set-Content -Path $_
-    }
+Get-ChildItem -Include AssemblyInfo.cs -Recurse | ForEach-Object {
+	$_.IsReadOnly = $false
+        (Get-Content -Path $_) -replace '(?<=Assembly(?:File)?Version\(")[^"]*(?="\))', $NewVersion |Set-Content -Path $_
+}
